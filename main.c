0d66a3b6943fa1a550bc0e1e637c7e1ba0bb7f54
Dev2 <dev2@example.com>
2024-03-18 02:43:21 +0000

    modified main.c

--- main.c
+++ main.c
@@ -1,59 +1,52 @@
 #include<stdio.h>
 main()
 {
-    printf("\n\n\tWelcome to Manoj's C program!\n\n);
-    int choice;
-    unsigned long int fact;
+	printf("\n\n\tWelcome to Manoj's C program!\n\n");
+	int c;
 
-    while(1)
-    {
-        printf("1. Factorial of a number\n");
-        printf("2. Biggest of 2 numbers\n");
-        printf("3. Biggest of 3 numbers\n");
-	printf("4. Pallindrome\n");
-	printf("5. Sum of 2 number\n");
-	printf("6. Reverse\n");
-	printf("7. Fibonaci\n");
-	printf("8. Sort numbers\n");
-        printf("9. Exit\n\n\n");
-        printf("Enter your choice :  ");
-        scanf("%d",&choice);
+ 	while(1)
+	{
+		printf("1. Factorial of a number\n");
+		printf("2. Biggest of 2 numbers\n");
+		printf("3. Biggest of 3 numbers\n");
+		printf("4. Pallindrome\n");
+		printf("5. Sum of 2 number\n");
+		printf("6. Reverse\n");
+		printf("7. Fibonaci\n");
+		printf("8. Sort numbers\n");
+		printf("9. Exit\n\n\n");
+		printf("Enter your choice :  ");
+		scanf("%d",&c);
 
-	switch(choice)
-	(
-		case 1:
-			fact ();
-			break;
-
-                case 2:
-                        big2 ();
-                        break;
-
-                case 3:
-                        big3 ();
-                        break;
-
-                case 4:
-                        pall ();
-                        break;
-                case 5:
-                        sum2 ();
-                        break;
-
-                case 6:
-                        rev ();
-                        break;
-
-                case 7:
-                        fib ();
-                        break;
-
-                case 8:
-                        sort ();
-                        break;
-
-                case 9:
-                        printf ("\n\nThank you. Until next time");
-                        break;
+	if (c == 1) {
+		fact ();
+		break;
+	} else if (c == 2) {
+		big2 ();
+		break;
+	} else if (c == 3) {
+		big3 ();
+		break;
+	} else if (c == 4) {
+		pall ();
+		break;
+	} else if (c == 5) {
+		sum2 ();
+		break;
+	} else if (c == 6) {
+		rev ();
+		break;
+	} else if (c == 7) {
+		fib ();
+		break;
+	} else if (c == 8) {
+		sort ();
+		break;
+	} else if (c == 9) {
+		printf ("\n\nThank you. Until next time");
+		exit(0);
+	} else {
+		printf ("\nInvalid option. Please try again.");
+	}
 	}
 }
